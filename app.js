6782bb6593f813ef55cfbaaad42b2d28bfe6d475
Betina9 <ob042025@gmail.com>
2025-11-13 14:35:31 +0100

    Fikk inn siste side av cookien med gif og navn

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const modal = document.getElementById("modal");
 const modalCloseBtn = document.getElementById("modal-close-btn");
-const form = document.getElementById("contact-form");
+const consentForm = document.getElementById("consent-form");
 const modalText = document.getElementById("modal-text");
 
 setTimeout(function () {
@@ -11,28 +11,30 @@ modalCloseBtn.addEventListener("click", function () {
   modal.style.display = "none";
 });
 
-form.addEventListener("submit", function (e) {
+consentForm.addEventListener("submit", function (e) {
   e.preventDefault();
+
+  const consentFormData = new FormData(consentForm);
+  const fullName = consentFormData.get("fullName");
+
   modalText.innerHTML = `
-  <div class="modal-inner-loading">
-    <img src="images/loading.svg" class="loading">
-    <p id="uploadText">
-        Uploading your data to the dark web...
-    </p>
-</div>`;
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>`;
 
   setTimeout(function () {
-    document.getElementById("upload-text").innerText = `Making the sale...`;
+    document.getElementById("upload-text").innerText = `
+        Making the sale...`;
   }, 1500);
 
   setTimeout(function () {
-    document.getElementById(
-      "modal-inner"
-    ).innerHTML = `<h2>Thanks you sucker! </h2>
-    <p>We just sold the rights to your eternal soul.</p>
-    <div class="idiot-gif">
-        <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNDRlazJpbHZ4OXAwNHhxM3p2Nm5iZWNrcHBkYzkzaWFoeDA5dmM5NCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/11C5ohOOaxjlcc/giphy.gif">
-    </div>
+    document.getElementById("modal-inner").innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
     `;
   }, 3000);
 });
