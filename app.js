70c7d18d6755d0e8a24bc78ec849a7a14d53b55e
Betina9 <ob042025@gmail.com>
2025-11-13 11:15:10 +0100

    lagt inn loading på cookie

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const modal = document.getElementById("modal");
 const modalCloseBtn = document.getElementById("modal-close-btn");
+const form = document.getElementById("contact-form");
 
 setTimeout(function () {
   modal.style.display = "inline";
@@ -7,4 +8,18 @@ setTimeout(function () {
 
 modalCloseBtn.addEventListener("click", function () {
   modal.style.display = "none";
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  modalText.innerHTML = `
+  <div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+</div>`;
+  setTimeout(function () {
+    document.getElementById("upload-text").innerText = `Making the sale...`;
+  }, 1500);
 });
